5aa21551cc292dd6b5c93ff123b8940bdb6ca733
Christian Plesner Hansen <c7n@p5r.org>
2014-06-10 15:14:04 +0200

    Missed one

---
+++ src/c/disass/disassembler-decoder-x86.c
@@ -0,0 +1,11 @@
+//- Copyright 2014 the Neutrino authors (see AUTHORS).
+//- Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+#define __STDC_LIMIT_MACROS
+#define __STDC_CONSTANT_MACROS
+#define NDEBUG 1
+
+#include <sys/types.h>
+#include "utils/types.hh"
+
+#include "X86DisassemblerDecoder.c"
