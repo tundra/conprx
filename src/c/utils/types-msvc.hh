eee90e8c771425706b1361b54819d0520a8e25f8
Christian Plesner Hansen <c7n@p5r.org>
2014-08-13 11:47:02 +0200

    Updated deps.

--- src/c/utils/types-msvc.hh
+++ src/c/utils/types-msvc.hh
@@ -3,33 +3,4 @@
 
 /// Console-related definitions for windows.
 
-#pragma warning(push, 0)
-#include <windows.h>
-#pragma warning(pop)
-
-#define dword_t DWORD
-#define word_t WORD
-#define short_t SHORT
-#define ushort_t USHORT
-#define win_size_t SIZE_T
-#define module_t HMODULE
-#define hwnd_t HWND
-#define bool_t BOOL
-#define ntstatus_t NTSTATUS
-#define ulong_t ULONG
-#define uint_t UINT
-
-#define ansi_char_t CHAR
-#define ansi_str_t LPSTR
-#define ansi_cstr_t LPCSTR
-
-#define wide_char_t WCHAR
-#define wide_str_t LPWSTR
-#define wide_cstr_t LPCWSTR
-
-#define str_t LPTSTR
-#define cstr_t LPCTSTR
-#define char_t TCHAR
-
-#define ssize_t SSIZE_T
-#define hkey_t HKEY
+#include "winhdr.h"
